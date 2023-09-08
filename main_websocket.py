2f37b793fcb12fbdcbcb5b0a7587945074f9c593
ghadaAy <ayatghada@gmail.com>
2023-09-08 17:16:55 +0100

    feat(live testing of the websocket)

--- main_websocket.py
+++ main_websocket.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, WebSocket
+from venv import logger
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from src.schema  import User
 from src.prompts import summarize_prompt, openai_prompt_template
 from settings import get_settings
 from src.llms.openai import OpenAIManager
 from src.prompts import summarize_prompt, openai_prompt_template
-
+import uvicorn
 app_settings = get_settings()
 app = FastAPI()
 
@@ -14,18 +15,24 @@ openai_qa = OpenAIManager(prompt=openai_prompt_template)
 
 
 app = FastAPI()
+websocket_clients = []
 
-connected_user = {}
-@app.websocket("/add_user/{user_id}")
-async def ask_for_summarization(websocket:WebSocket, user:User):
+@app.websocket("/summarization/")
+async def ask_for_summarization(websocket:WebSocket):
     await websocket.accept()
-    connected_user[user.id_] = websocket
-    while True:
-        message = await websocket.receive_text()
-        async for token in openai_summarizing.answer(message):
-            await websocket.send_text(token)
-        await websocket.close()
+    websocket_clients.append(websocket)
+    try:
+        while True:
+            message = await websocket.receive_text()
+            async for token in openai_summarizing.answer(message):
+                await websocket.send_text(token)
+    except WebSocketDisconnect:
+        logger.info("websocket out")
+        websocket_clients.remove(websocket)
+    
 
+if __name__ == "__main__":
+    uvicorn.run("main_websocket:app", port=8080)
 
 
 
