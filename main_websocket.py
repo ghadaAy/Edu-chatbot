8658a0f593a744bdf4b25cbb2aa8bb05a6dae2a8
ghadaAy <ayatghada@gmail.com>
2023-09-12 17:19:05 +0100

    fix(websocket fix to stream for summarization and qa)

--- main_websocket.py
+++ main_websocket.py
@@ -1,6 +1,5 @@
 from venv import logger
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from src.schema  import User
+from fastapi import FastAPI, Query, WebSocket, WebSocketDisconnect
 from src.prompts import summarize_prompt, openai_prompt_template
 from settings import get_settings
 from src.llms.openai import OpenAIManager
@@ -17,8 +16,8 @@ openai_qa = OpenAIManager(prompt=openai_prompt_template)
 app = FastAPI()
 websocket_clients = []
 
-@app.websocket("/ws")
-async def ask_for_summarization(websocket:WebSocket):
+@app.websocket("/summarizing")
+async def ask_for_summarization(websocket:WebSocket, user_id:str=Query(...)):
     
     try:
         await websocket.accept()
@@ -26,9 +25,24 @@ async def ask_for_summarization(websocket:WebSocket):
         while True:
             
             message = await websocket.receive_text()
-            async for token in openai_summarizing.answer(message):
+            async for token in openai_summarizing.run_qa_chain(message, user_id):
                 await websocket.send_text(token)
-                print(token)
+    except WebSocketDisconnect:
+        logger.info("websocket out")
+        # websocket_clients.remove(websocket)
+    
+
+@app.websocket("/qa")
+async def ask_for_qa(websocket:WebSocket, user_id:str=Query(...)):
+    
+    try:
+        await websocket.accept()
+        websocket_clients.append(websocket)
+        while True:
+            
+            message = await websocket.receive_text()
+            async for token in openai_qa.run_qa_chain(message, user_id):
+                await websocket.send_text(token)
     except WebSocketDisconnect:
         logger.info("websocket out")
         # websocket_clients.remove(websocket)
@@ -36,6 +50,3 @@ async def ask_for_summarization(websocket:WebSocket):
 
 if __name__ == "__main__":
     uvicorn.run("main_websocket:app", port=8080)
-
-
-
